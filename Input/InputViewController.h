1ff5fef3910d6f24664c55d93819b2f8330e467d
DongRanSun <sundr105@163.com>
2021-06-25 17:37:17 +0800

    Add files via upload

---
+++ Input/InputViewController.h
@@ -0,0 +1,18 @@
+//
+//  InputViewController.h
+//  testPodDemo
+//
+//  Created by sundongran on 2021/6/25.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface InputViewController : UIViewController
+
+- (int)number:(int)a;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Input/InputViewController.m
@@ -0,0 +1,35 @@
+//
+//  InputViewController.m
+//  testPodDemo
+//
+//  Created by sundongran on 2021/6/25.
+//
+
+#import "InputViewController.h"
+
+@interface InputViewController ()
+
+@end
+
+@implementation InputViewController
+
+- (int)number:(int)a{
+    return a++;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
